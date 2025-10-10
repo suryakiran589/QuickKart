16966d706ab09ff12b8e689b3ab9c10fbf3f0f00
surya <kommojusurya61@gmail.com>
2025-10-10 18:55:45 +0530

    cleaned unused code

--- src/components/RatingFilters.tsx
+++ src/components/RatingFilters.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { filters } from "../constants"
 
 
-const RatingFilters = ({selectedRating,setSelectedRating}:{selectedRating:string,setSelectedRating:any}) => {
+const RatingFilters = ({setSelectedRating}:{selectedRating:string,setSelectedRating:any}) => {
     const [isOpen,setOpen] = useState(true)
   return (
    <div className="border-b-gray-100 border-b-2 pb-2 pl-3 mb-2">
@@ -14,7 +14,7 @@ const RatingFilters = ({selectedRating,setSelectedRating}:{selectedRating:string
    
          { filters.ratings.map((rating) =>{
            return (<div className="flex items-center pb-2">
-             <input type="checkbox" name={rating}  value={rating} onChange={(e)=>setSelectedRating((prev) => prev ==e.target.value ? "" :e.target.value)}/>
+             <input type="checkbox" name={rating}  value={rating} onChange={(e)=>setSelectedRating((prev:string) => prev ==e.target.value ? "" :e.target.value)}/>
              <label htmlFor={rating} className="text-sm font-medium pl-4">{`${rating}★ & Above`}</label>
            </div>)
          })}
